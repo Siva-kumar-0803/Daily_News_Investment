885c8f8c60daac5c5ea5193705094a35fa9873fa
Sivakkumar <makemoney9047@gmail.com>
2025-05-25 12:02:37 +0530

    test the code .

---
+++ app.py
@@ -0,0 +1,58 @@
+import pywhatkit
+import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+driver = webdriver.Chrome()
+
+URL = "https://www.goodreturns.in/gold-rates/chennai.html"
+phone_number = "+919361958447"
+hour = 11
+minute = 2                                                                                                                                    
+message = ""
+
+def scrap_gold_prices():
+    global message
+
+    driver.get(URL)
+
+    time.sleep(2)  # Wait for the page to load
+
+    gold_prices = driver.find_elements(By.CLASS_NAME, "gold-common-head")
+
+    for price in gold_prices:
+        print(price.text)
+        message += price.text +"\n"
+      
+
+
+def send_whatsapp_message(phone_number, message, hour, minute):
+        
+        try:
+            pywhatkit.sendwhatmsg(phone_number, message, hour, minute)
+            print(f"Message scheduled to be sent to {phone_number} at {hour}:{minute}.")
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+now = datetime.datetime.now()
+
+
+def main():
+    if now.strftime("%p") == "AM":
+        scrap_gold_prices()
+        driver.quit()  
+
+        print("Gold prices scraped successfully.")
+
+        send_whatsapp_message(phone_number, message, hour, minute)     
+
+    else :
+        print("It's not AM, so the message will not be sent.")
+        # You can add any other logic here if needed   
+
+        if __name__ == "__main__":
+             main()
